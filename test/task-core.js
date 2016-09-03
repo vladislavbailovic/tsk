1f98aacf4e32ebb23edb680c46e6ee829b58c41f
vladislav <ve@incsub.com>
2016-09-03 08:43:06 +0200

    Start on core tests.

---
+++ test/task-core.js
@@ -0,0 +1,24 @@
+var tap = require('tap');
+
+tap.test('task/core.js', function (t) {
+
+	var Task = require('../lib/task/core'),
+		tsk = require('../lib/util')
+	;
+
+	t.test('task.load', function (t) {
+		// Shim the path getter
+		tsk.get_path = function () { return false; };
+
+		var task = new Task();
+		var prm = task.load('something').then(function (smt) {
+			t.notOk(smt, "Shimmed path returns false-ish");
+		});
+		t.ok(prm instanceof Promise, "Load returns a promise");
+
+		t.end();
+	});
+
+	t.end();
+
+});
