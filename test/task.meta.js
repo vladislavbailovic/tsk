cc9eaebcb165e141672fe1fab3018351f3896fbd
vladislav <ve@incsub.com>
2016-09-04 10:43:24 +0200

    Refactor task data validation.

--- test/task.meta.js
+++ test/task.meta.js
@@ -9,11 +9,25 @@ tap.test('task/core.js', function (t) {
 
 	t.test('task.get_valid_meta', function (t) {
 		var task = new Task(),
-			tstamp = tsk.date.fmt()
+			tstamp = tsk.date.fmt(),
+			tmp
 		;
 
-		t.equal(task.get_valid_meta('created', tstamp), tstamp, "Valid created date with existing value");
+		t.equal(task.get_valid_meta('created', tstamp), tstamp, "Valid created date with valid existing value");
 		t.match(task.get_valid_meta('created', 'aaa'), /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/, 'Invalid created date converts to match date regex');
+		t.match(task.get_valid_meta('created', false), /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/, 'Empty created date converts to match date regex');
+
+		t.equal(task.get_valid_meta('modified', tstamp), tstamp, "Valid modified date with valid existing value");
+		t.match(task.get_valid_meta('modified', 'aaa'), /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/, 'Invalid modified date converts to match date regex');
+		t.match(task.get_valid_meta('modified', false), /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/, 'Empty modified date converts to match date regex');
+
+		tmp = task.get_valid_meta('followers', false);
+		t.ok(tmp instanceof Array, "Empty followers becomes an array");
+		t.ok(0 === tmp.length, "Empty followers becomes an *empty* array");
+
+		tmp = task.get_valid_meta('followers', "aaa");
+		t.ok(tmp instanceof Array, "Invalid followers become an array");
+		//t.ok(0 === tmp.length, "Invalid followers become an *empty* array");
 
 		t.end();
 	});
