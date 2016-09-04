e26cbc2d4a4eafe9e442e6591e8ebf38d29c711e
vladislav <ve@incsub.com>
2016-09-04 09:56:21 +0200

    Test task core saves.

--- test/task.core.js
+++ test/task.core.js
@@ -2,21 +2,77 @@ var tap = require('tap');
 
 tap.test('task/core.js', function (t) {
 
-	var Task = require('../lib/task/core'),
-		tsk = require('../lib/util')
-	;
+	var Task = require('../lib/task/core');
 
 	t.test('task.load', function (t) {
+		var tsk = require('../lib/util');
+
 		// Shim the path getter
-		tsk.get_path = function () { return false; };
+		tsk.get_path = function () { return 'nothing'; };
+		Task.prototype.get_meta_object = function () { return {}; };
 
 		var task = new Task();
 		var prm = task.load('something').then(function (smt) {
-			t.notOk(smt, "Shimmed path returns false-ish");
+			return t.test('task.load.promise', function (t) {
+				t.notOk(smt.task, "Shimmed path returns empty task");
+				t.end();
+			});
 		});
 		t.ok(prm instanceof Promise, "Load returns a promise");
 
-		t.end();
+		return prm;
+	});
+
+	t.test('task.save reject', function (t) {
+		var fs = require('fs');
+
+		// Shim file writing to throw error
+		fs.writeFile = function (file, data, callback) {
+			callback.apply(fs, [true]);
+		};
+
+		var task = new Task();
+		var prm = task.save('something')
+			.then(
+				function () {
+					t.fail("Should have been rejected");
+					t.end();
+				},
+				function (smt) {
+					t.pass("Rejected");
+					t.end();
+				}
+			)
+		;
+		t.ok(prm instanceof Promise, "Save returns a promise");
+
+		return prm;
+	});
+
+	t.test('task.save accept', function (t) {
+		var fs = require('fs');
+
+		// Shim file writing to throw error
+		fs.writeFile = function (file, data, callback) {
+			callback.apply(fs, [false]);
+		};
+
+		var task = new Task();
+		var prm = task.save('something')
+			.then(
+				function () {
+					t.pass("Accepted");
+					t.end();
+				},
+				function (smt) {
+					t.fail("Should have been accepted");
+					t.end();
+				}
+			)
+		;
+		t.ok(prm instanceof Promise, "Save returns a promise");
+
+		return prm;
 	});
 
 	t.end();
