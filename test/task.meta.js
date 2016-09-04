72660b65481c53363faa2c071597a8d2f2eb8fc0
vladislav <ve@incsub.com>
2016-09-04 11:11:28 +0200

    Add more meta tests.

--- test/task.meta.js
+++ test/task.meta.js
@@ -33,6 +33,39 @@ tap.test('task/core.js', function (t) {
 		t.ok(tmp instanceof Array, "Invalid CSV followers become an array");
 		t.ok(0 === tmp.length, "Invalid CSV followers become an *empty* array");
 
+		tmp = task.get_valid_meta('unknown whatever', "aaa,bbb,ccc");
+		t.notOk(tmp, "Unknown key returns false-ish");
+
+		t.end();
+	});
+
+	t.test('task.get_meta_object', function (t) {
+		var task = new Task();
+
+		t.ok(tsk.data.is_valid_date(task.get_meta('created')), "Valid created date value");
+		t.ok(tsk.data.is_valid_array(task.get_meta('followers')), "Valid followers value");
+
+		t.end();
+	});
+
+	t.test('task.get_meta', function (t) {
+		var task = new Task(),
+			obj = {
+				created: tsk.date.fmt(),
+				followers: "some stuff here",
+				test_property: "Has some value"
+			},
+			result = task.get_meta_object(obj)
+		;
+
+		t.ok("created" in result, "Result retains created date");
+		t.equal(result.created, obj.created, "Created date value preserved");
+
+		t.ok("followers" in result, "Result retains followers");
+		t.notEqual(result.followers.length, obj.followers.length, "Followers value validated");
+
+		t.notOk("test_property" in result, "Result does not retain unknown property");
+
 		t.end();
 	});
 
